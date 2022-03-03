748cc687208f1638e12426998f85a523299d075e
CodeLikeKitten <38568309+CodeLikeKitten@users.noreply.github.com>
2022-03-03 14:51:41 +0400

    Update script.js

--- lesson01/script.js
+++ lesson01/script.js
@@ -1,7 +1,7 @@
-let money;
-let addExpenses;
-let deposit;
-let mission;
-let period;
+let money = null;
+let addExpenses = null;
+let deposit = null;
+let mission = null;
+let period = null;
 alert("КРИЧУ");
 console.log("Любой текст");
