f0be93cbfd7f09e4c0bbe2ff4b2270f4b81e83f4
CodeLikeKitten <CodeLikeKitten@github.com>
2022-03-31 15:00:04 +0400

    Добавил цикл

--- lesson03/scripts.js
+++ lesson03/scripts.js
@@ -6,8 +6,13 @@ let income = 'фриланс';
 let addExpenses = prompt("Перечислите ваши расходы через запятую:").split(',');
 let deposit = !!prompt("Есть ли у вас депозит в банке?");
 let expenses = prompt("Введите через запятую обязательные статьи расходов: ").split(',');
+let amount = prompt("Во сколько это обойдётся? (также через запятую)").split(',');
 let mission = 15000;
 let period = 10;
+let budgetMonth = 0;
+for (let i = 0; i < expenses.length;i++){
+    budgetMonth+=amount[i];
+}
 console.log(money+ ' '+ income+ ' '+ deposit);
 
 console.log('Типы данных: ', typeof(money),typeof(income), typeof(deposit));
