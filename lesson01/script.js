39285b8966fc9ced755cf2af118cb38d4e905cfc
CodeLikeKitten <CodeLikeKitten@github.com>
2022-04-15 01:49:45 +0400

    Закончил урок

--- lesson01/script.js
+++
@@ -1,7 +0,0 @@
-let money = null;
-let addExpenses = null;
-let deposit = null;
-let mission = null;
-let period = null;
-alert("КРИЧУ");
-console.log("Любой текст");
--- lesson03/scripts.js
+++ lesson03/scripts.js
@@ -1,22 +1,54 @@
 'use strict'
 
   
-let money = prompt("Ваш ежемесячный доход: ", 1000);
+let money = parseInt(prompt("Ваш ежемесячный доход: ", 1000),10);
 let income = 'фриланс';
 let addExpenses = prompt("Перечислите ваши расходы через запятую:").split(',');
-let deposit = !!prompt("Есть ли у вас депозит в банке?");
+let deposit = prompt("Есть ли у вас депозит в банке?");
+if (deposit.toLocaleLowerCase() == "да")
+{
+    deposit = true;
+}
+else
+{
+    deposit = false;
+}
 let expenses = prompt("Введите через запятую обязательные статьи расходов: ").split(',');
 let amount = prompt("Во сколько это обойдётся? (также через запятую)").split(',');
 let mission = 15000;
 let period = 10;
-let budgetMonth = 0;
-for (let i = 0; i < expenses.length;i++){
-    budgetMonth+=amount[i];
+let budgetMonth = money;
+// for (let i = 0; i < addExpenses.length;i++)
+// {
+//     budgetMonth+=parseInt(addExpenses[i],10);
+// }
+for (let i = 0; i < amount.length;i++){
+    budgetMonth-=parseInt(amount[i],10);
 }
-console.log(money+ ' '+ income+ ' '+ deposit);
+//console.log(money+ ' '+ income+ ' '+ deposit);
 
 console.log('Типы данных: ', typeof(money),typeof(income), typeof(deposit));
 console.log('Период равен '+period+' месяцев');
-console.log('Цель заработать '+ mission+ ' рублей');
-let budgetDay = money / 30;
-console.log(budgetDay);
+console.log('Цель заработать '+ mission+ ' рублей; Будет достигнута через '+ Math.ceil(mission/budgetMonth)+' месяцев');
+let budgetDay = budgetMonth / 30;
+console.log(Math.floor(budgetDay));
+
+if (budgetDay>1200)
+{
+    console.log('У вас высокий уровень дохода');
+}
+
+if ((budgetDay>600) && (budgetDay<=1200))
+{
+    console.log('У вас средний уровень дохода');
+}
+
+if (budgetDay<=600)
+{
+    console.log('К сожалению, у вас уровень дохода ниже среднего');
+}
+
+if (budgetDay < 0)
+{
+    console.log('Что-то пошло не так');
+}
